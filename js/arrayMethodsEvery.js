6725fb40f3b02a81ece6448eca361ba0f882d463
unknown <jacobzff77@gmail.com>
2023-04-04 01:33:35 +0100

    every()

---
+++ js/arrayMethodsEvery.js
@@ -0,0 +1,41 @@
+// every() 
+
+// the every method if all array elements pass the given test function
+
+// function that checks wether the age is 18 or above
+
+function checkAdult(age){
+    return age >= 18;
+}
+
+const ageArray = [34,34,20,26,12];
+
+// checks if all array element pass the checkAdult() function
+
+let check = ageArray.every(checkAdult);
+
+console.log(check);
+
+
+// check if array elements are even 
+
+// function that checks wether all the array elements are even or not
+
+function checkEven(num){
+    return num % 2 === 0;
+}
+
+const numbers = [2,4,6,,7,8];
+
+let check2= numbers.every(checkEven);
+
+console.log(check2); //falses
+
+// every with arrow functions
+
+let nums = [1,2,3,4];
+
+// use arrow function with every()
+
+let result = nums.every(element => element < 6);
+console.log(result);
